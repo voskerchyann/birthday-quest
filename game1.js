60c35395abb5c50e2a2e9392ec1c8eca7a7db8a6
voskerchyann <voskerchyanliana348@gmail.com>
2026-04-22 23:44:20 +0400

    Create game1.js

---
+++ game1.js
@@ -0,0 +1,9 @@
+let correct = Math.floor(Math.random() * 9) + 1;
+
+function check(num) {
+    if (num === correct) {
+        alert("OOOOO MALADEEECCCC 🎉");
+    } else {
+        alert("chkpav....");
+    }
+}
